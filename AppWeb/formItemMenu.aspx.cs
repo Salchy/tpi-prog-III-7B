c96370486b4e7989ead0550e4dd1d32299b46a29
FrancoCapurro <franco.capurro@hotmail.com>
2025-06-06 17:15:31 -0300

    agrego getCategorias + avances formItem

--- AppWeb/formItemMenu.aspx.cs
+++ AppWeb/formItemMenu.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace AppWeb
 {
@@ -11,7 +13,17 @@ namespace AppWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            CategoriasDatos lista = new CategoriasDatos();
+            ddlCategoria.DataSource = lista.listarCategorias();
+            ddlCategoria.DataTextField = "Nombre";     // VISUAL
+            ddlCategoria.DataValueField = "Id";        // QUE SE GUARDA
+            ddlCategoria.DataBind();
+
+
+            ddlCategoria.Items.Insert(0, new ListItem("-- Seleccione --", "0")); // PREDETERMINADO
 
         }
+
+       
     }
 }
---
+++ negocio/CategoriasDatos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class CategoriasDatos
+    {
+        Database database;
+        public List<Categoria> listarCategorias()
+        {
+
+            List<Categoria> lista = new List<Categoria>();
+            database = new Database();
+
+            try
+            {
+                database.setQuery("SELECT id_Categoria, Nombre_Categoria From Categoria_Menu");
+                database.execQuery();
+
+
+                while (database.Reader.Read())
+
+                {
+                    Categoria cate = new Categoria(
+                Convert.ToInt32(database.Reader["id_Categoria"]),
+                database.Reader["Nombre_Categoria"].ToString());
+                    
+                        lista.Add(cate);
+
+
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                database.closeConnection();
+
+            }
+
+
+        }
+    }
+
+}
