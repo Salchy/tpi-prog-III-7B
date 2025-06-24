e71dea4e5f1b3aaa096b31748fc33dd0c51c38e7
Salchy <leandrocg524@gmail.com>
2025-06-23 22:05:47 -0300

    Login y Logout + Validaciones de login y niveles desde MasterPage

--- AppWeb/masterPageGerencia.Master.cs
+++ AppWeb/masterPageGerencia.Master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,23 @@ namespace AppWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!UsuarioDatos.SesionActiva(Session["Usuario"]))
+            {
+                Response.Redirect("login.aspx", false);
+                return;
+            }
+            if (UsuarioDatos.GetLevel(Session["Usuario"]) > 1)
+            {
+                Session.Add("error", "Mensaje de error");
+                Response.Redirect("Error.aspx", false);
+                return;
+            }
+        }
 
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("Login.aspx");
         }
     }
 }
